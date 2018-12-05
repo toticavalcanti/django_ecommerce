af9ff58ac3509fc2035eb8d43bf4ed7656289fc5
toti.cavalcanti <toticavalcanti@hotmail.com>
2018-12-05 12:05:24 -0200

    create context title and content to each page

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -1,4 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 def home_page(request):
-    return render(request, "home_page.html", {})
+    context = {
+        "title": "Página principal"
+        "content": "Bem-vindo a página principal"
+    }
+    return render(request, "home_page.html", context)
+
+def about_page(request):
+    context = {
+        "title": "Página sobre"
+        "content": "Bem-vindo a página sobre"
+    }
+    return render(request, "about_page.html", context)
+
+def contact_page(request):
+    context = {
+        "title": "Página de contato"
+        "content": "Bem-vindo a página de contato"
+    }
+    return render(request, "contact_page.html", context)
