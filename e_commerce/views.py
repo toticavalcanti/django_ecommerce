07ea157ee5bd3e175bace913c9ce711651e8367b
toti.cavalcanti <toticavalcanti@hotmail.com>
2018-12-05 13:59:24 -0200

    create context variable title and content to home_page, about_page and contact_page, create and set src/template/about/views.html, src/template/contact/view.html and update src/template/home_page.html to use context variables

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -2,21 +2,21 @@ from django.http import HttpResponse
 from django.shortcuts import render
 def home_page(request):
     context = {
-        "title": "Página principal"
+        "title": "Página principal",
         "content": "Bem-vindo a página principal"
     }
     return render(request, "home_page.html", context)
 
 def about_page(request):
     context = {
-        "title": "Página sobre"
+        "title": "Página sobre",
         "content": "Bem-vindo a página sobre"
     }
     return render(request, "about/view.html", context)
 
 def contact_page(request):
     context = {
-        "title": "Página de contato"
+        "title": "Página de contato",
         "content": "Bem-vindo a página de contato"
     }
     return render(request, "contact/view.html", context)
