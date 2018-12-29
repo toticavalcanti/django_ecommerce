7f84e57391cde680fa400a8bdfdac416ecacd813
toti.cavalcanti <toticavalcanti@hotmail.com>
2018-12-29 13:45:38 -0200

    insert __str__ method in src/products/models.py and run migrations

--- products/models.py
+++ products/models.py
@@ -5,3 +5,11 @@ class Product(models.Model): #product_category
     title       = models.CharField(max_length=120)
     description = models.TextField()
     price       = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
+
+    #python 3
+    def __str__(self):
+        return self.title
+        
+    #python 2
+    def __unicode__(self):
+        return self.title
