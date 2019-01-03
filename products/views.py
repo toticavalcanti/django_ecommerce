5f456e79e4e8293507a8fb9bb80d24a1523b40d5
toti.cavalcanti <toticavalcanti@hotmail.com>
2019-01-03 01:32:26 -0200

    update products/templates/products/list.html and products/views.py

--- products/views.py
+++ products/views.py
@@ -9,10 +9,16 @@ class ProductListView(ListView):
     queryset = Product.objects.all()
     template_name = "products/list.html"
 
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
+    #     print(context)
+    #     return context
+
+
 #Function Based View
 def product_list_view(request):
     queryset = Product.objects.all()
     context = {
-        'qs': queryset
+        'object_list': queryset
     }
     return render(request, "products/list.html", context)
