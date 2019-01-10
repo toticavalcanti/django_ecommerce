29c867135edc7be87ece841b4890e64393404149
toti.cavalcanti <toticavalcanti@hotmail.com>
2019-01-10 15:55:25 -0200

    insert image field in src/products/models.py

--- products/models.py
+++ products/models.py
@@ -5,6 +5,7 @@ class Product(models.Model): #product_category
     title       = models.CharField(max_length=120)
     description = models.TextField()
     price       = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
+    image       = models.FileField(upload_to = 'products/')
 
     #python 3
     def __str__(self):
