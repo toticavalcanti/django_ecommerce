a4bfb0a1b2651c017abbba1e87a0cabae4eb9441
toti.cavalcanti <toticavalcanti@hotmail.com>
2019-01-17 11:59:34 -0200

    optimize product_detail_view in src/products/views.py

--- products/views.py
+++ products/views.py
@@ -1,3 +1,5 @@
+from django.http import Http404
+
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
 
@@ -36,7 +38,13 @@ class ProductDetailView(DetailView):
 #Function Based View
 def product_detail_view(request, pk = None, *args, **kwargs):
     #instance = Product.objects.get(pk = pk) #get the object id
-    instance = get_object_or_404(Product, pk = pk)
+    #instance = get_object_or_404(Product, pk = pk)
+    qs = Product.objects.filter(id = pk)
+    if qs.count() == 1:
+        instance = qs.first()
+    else:
+        raise Http404("Esse produto não existe!")
+
     context = {
         'object': instance
     }
