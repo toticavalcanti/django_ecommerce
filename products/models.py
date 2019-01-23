9f0ddb49624376ad81797c555b35fc2b7c44c6a3
toti.cavalcanti <toticavalcanti@hotmail.com>
2019-01-23 14:20:39 -0200

    Create custom ProductManager class in src/products/models.py

--- products/models.py
+++ products/models.py
@@ -1,16 +1,24 @@
 from django.db import models
 
+class ProductManager(models.Manager):
+    def get_by_id(self, id):
+        qs = self.get_queryset().filter(id = id)
+        if qs.count() == 1:
+            return qs.first()
+        return None
+
 # Create your models here.
 class Product(models.Model): #product_category
     title       = models.CharField(max_length=120)
     description = models.TextField()
     price       = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
-    image       = models.ImageField(upload_to = 'products/', null = True, blank = True)
+    image       = models.FileField(upload_to = 'products/', null = True, blank = True)
 
+    objects = ProductManager()
+    
     #python 3
     def __str__(self):
         return self.title
-        
     #python 2
     def __unicode__(self):
         return self.title
