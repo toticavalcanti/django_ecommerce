bde8f7b9632516a934ca3577138a32dc9f979c42
toti.cavalcanti <toticavalcanti@hotmail.com>
2019-02-07 18:02:33 -0200

    create ProductQuerySet class and define active and featured method inside this class, extend get_queryset method, create all method and featured method inside ProductManager class in src/products/models.py

--- products/models.py
+++ products/models.py
@@ -1,6 +1,26 @@
 from django.db import models
 
+#Custom queryset
+class ProductQuerySet(models.query.QuerySet):
+    
+    def active(self):
+        return self.filter(active = True)
+
+    def featured(self):
+        return self.filter(featured = True, active = True)
+
 class ProductManager(models.Manager):
+    
+    def get_queryset(self):
+        return ProductQuerySet(self.model, using = self._db)
+    
+    def all(self):
+        return self.get_queryset().active()
+
+    def featured(self):
+        #self.get_queryset().filter(featured = True)
+        return self.get_queryset().featured()
+
     def get_by_id(self, id):
         qs = self.get_queryset().filter(id = id)
         if qs.count() == 1:
@@ -13,6 +33,9 @@ class Product(models.Model): #product_category
     description = models.TextField()
     price       = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
     image       = models.FileField(upload_to = 'products/', null = True, blank = True)
+    featured    = models.BooleanField(default = False)
+    active      = models.BooleanField(default = True)
+
 
     objects = ProductManager()
     
