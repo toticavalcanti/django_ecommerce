6e69988db60c91c0824dc3cc8970e3b501ffe5f1
toti.cavalcanti <toticavalcanti@hotmail.com>
2019-02-19 17:54:01 -0300

    set unique = True in slug field

--- products/models.py
+++ products/models.py
@@ -30,7 +30,7 @@ class ProductManager(models.Manager):
 # Create your models here.
 class Product(models.Model): #product_category
     title       = models.CharField(max_length=120)
-    slug        = models.SlugField()
+    slug        = models.SlugField(unique = True)
     description = models.TextField()
     price       = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
     image       = models.FileField(upload_to = 'products/', null = True, blank = True)
