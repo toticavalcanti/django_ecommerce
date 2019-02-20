3a6e19b19e5371d8079f253ce8851eb498e3e573
toti.cavalcanti <toticavalcanti@hotmail.com>
2019-02-20 11:31:55 -0300

    create src/products/utils.py and update src/products/models.py

--- products/models.py
+++ products/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from .utils import unique_slug_generator
-from django.db.models.signals import pre_save
 
 #Custom queryset
 class ProductQuerySet(models.query.QuerySet):
@@ -49,9 +47,3 @@ class Product(models.Model): #product_category
     #python 2
     def __unicode__(self):
         return self.title
-
-    def product_pre_save_receiver(sender, instance, *args, **kwargs):
-    if not instance.slug:
-        instance.slug = unique_slug_generator(instance)
-
-    pre_save.connect(product_pre_save_receiver, sender = Product)
