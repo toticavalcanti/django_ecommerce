2645f536be565b3447b404471f8ed3d2d1991d47
toti.cavalcanti <toticavalcanti@hotmail.com>
2019-02-20 13:13:29 -0300

    insert blank = True in slug field in Product class in src/products/models.py

--- products/models.py
+++ products/models.py
@@ -32,7 +32,7 @@ class ProductManager(models.Manager):
 # Create your models here.
 class Product(models.Model): #product_category
     title       = models.CharField(max_length=120)
-    slug        = models.SlugField(unique = True)
+    slug        = models.SlugField(blank = True, unique = True)
     description = models.TextField()
     price       = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
     image       = models.FileField(upload_to = 'products/', null = True, blank = True)
