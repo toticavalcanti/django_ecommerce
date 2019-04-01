0ceddd175d60dad424fdb02f012f0efab52eb245
toti.cavalcanti <toticavalcanti@hotmail.com>
2019-04-01 15:35:00 -0300

    fix line 57 of src/e_commerce/settings.py, change BASE_DIR 'template' to 'templates

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'e_commerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'template')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
