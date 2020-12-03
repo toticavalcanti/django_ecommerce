c6ac40e46b7793314cffebee6e029ba13ed96a2a
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-12-03 16:41:51 -0300

    create cart model and run migrate

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
 def cart_home(request):
+    cart_id = request.session.get("cart_id", None)
+    if cart_id is None:
+        print('create new cart')
+        request.session['cart_id'] = 12
+    else:
+        print("Cart ID exists")
     return render(request, "carts/home.html", {} )
-
