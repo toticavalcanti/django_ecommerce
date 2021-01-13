19475d684a7966d973f7ccf72667c90ec440f2ed
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2021-01-12 22:36:30 -0300

    create cart_update in cart views

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+
+from products.models import Product
 from .models import Cart
 
 def cart_home(request):
     cart_obj = Cart.objects.new_or_get(request)
     return render(request, "carts/home.html", {})
+
+def cart_update(request):
+    product_id = 5
+    # Pega o produto com id 5
+    product_obj = Product.objects.get(id=product_id)
+    # Cria ou pega a instância já existente do carrinho
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    # E o produto se adiciona a instância do campo M2M 
+    cart_obj.products.add(product_obj) # cart_obj.products.add(product_id)
+    #cart_obj.products.remove(product_obj) # cart_obj.products.remove(product_id)
+    return redirect(product_obj.get_absolute_url())
