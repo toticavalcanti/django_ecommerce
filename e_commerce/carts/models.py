affbc06cbdca4795e891f51779ebaeead23065f9
toticavalcanti <toticavalcanti@hotmail.com>
2021-02-10 23:11:05 -0300

    fix pre_save_cart_receiver

--- e_commerce/carts/models.py
+++ e_commerce/carts/models.py
@@ -57,6 +57,9 @@ def m2m_changed_cart_receiver(sender, instance, action, *args, **kwargs):
 m2m_changed.connect(m2m_changed_cart_receiver, sender = Cart.products.through)
 
 def pre_save_cart_receiver(sender, instance, *args, **kwargs):
-    instance.total = instance.subtotal  + 10
+    if instance.subtotal > 0:
+        instance.total = instance.subtotal  + 10 # imagine o 10 como uma taxa de entrega
+    else:
+        instance.total = 0.00
 
 pre_save.connect(pre_save_cart_receiver, sender = Cart)
