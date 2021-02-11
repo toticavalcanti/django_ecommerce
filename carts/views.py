d35e5a8ea1f60830aeb218b10c4fde109e892f20
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2021-02-11 16:30:38 -0300

    Update views.py

--- carts/views.py
+++ carts/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Cart
 
 def cart_home(request):
-    cart_obj = Cart.objects.new_or_get(request)
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
     return render(request, "carts/home.html", {})
