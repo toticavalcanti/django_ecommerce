e18a072a7bc1c60cd761268f6bc91dc1a392e759
toticavalcanti <toticavalcanti@hotmail.com>
2021-03-02 19:28:56 -0300

    merge master with display-cart

--- e_commerce/carts/views.py
+++ e_commerce/carts/views.py
@@ -4,22 +4,21 @@ from products.models import Product
 from .models import Cart
 
 def cart_home(request):
-    cart_obj, new_obj = Cart.objects.new_or_get(request)
-    return render(request, "carts/home.html", {})
+    cart_obj, new_obj  = Cart.objects.new_or_get(request)
+    return render(request, "carts/home.html", {"cart": cart_obj})
 
 def cart_update(request):
+    print(request.POST)
     product_id = request.POST.get('product_id')
     if product_id is not None:
         try:
-            product_obj = Product.objects.get(id=product_id)
+            product_obj = Product.objects.get(id = product_id)
         except Product.DoesNotExist:
-            print("Mostra mensagem para o usuário, produto em falta?")
+            print("Mostrar mensagem ao usuário, esse produto acabou!")
             return redirect("cart:home")
-        cart_obj, new_obj = Cart.objects.new_or_get(request)
-        if product_obj in cart_obj.products.all():
-            cart_obj.products.remove(product_obj)
-        else:
-            cart_obj.products.add(product_obj) # cart_obj.products.add(product_id)
-        
-        # return redirect(product_obj.get_absolute_url())
+        cart_obj, new_obj = Cart.objects.new_or_get(request) 
+        if product_obj in cart_obj.products.all(): 
+            cart_obj.products.remove(product_obj) 
+        else: 
+            cart_obj.products.add(product_obj)
     return redirect("cart:home")
