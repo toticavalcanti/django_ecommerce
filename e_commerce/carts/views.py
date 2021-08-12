ef707ab838b2c7b3f3a17977f0dde132d6368264
toticavalcanti <toticavalcanti@hotmail.com>
2021-08-12 15:37:20 -0300

    imports accounts.forms from LoginForm and billing.models from BillingProfile in views.py

--- e_commerce/carts/views.py
+++ e_commerce/carts/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
+
+from accounts.forms import LoginForm
+from billing.models import BillingProfile
 from orders.models import Order
 from products.models import Product
 from .models import Cart
+
 
 def cart_home(request):
     cart_obj, new_obj  = Cart.objects.new_or_get(request)
@@ -35,4 +39,15 @@ def checkout_home(request):
     #aqui a order associada ao carrinho
     else:
         order_obj, new_order_obj = Order.objects.get_or_create(cart = cart_obj)
-    return render(request, "carts/checkout.html", {"object": order_obj})
+    user = request.user
+    billing_profile = None
+    login_form = LoginForm()
+    if user.is_authenticated:
+        billing_profile, billing_profile_created = BillingProfile.objects.get_or_create(user=user, email=user.email)
+
+    context = {
+        "object": order_obj,
+        "billing_profile": billing_profile,
+        "login_form": login_form
+    }
+    return render(request, "carts/checkout.html", context)
