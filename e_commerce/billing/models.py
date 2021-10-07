c8bf730058cc7d90092f81befabf921e1e88bcbc
toticavalcanti <toticavalcanti@hotmail.com>
2021-10-07 14:35:08 -0300

    billing profile manager

--- e_commerce/billing/models.py
+++ e_commerce/billing/models.py
@@ -1,9 +1,31 @@
 from django.conf import settings
 from django.db import models
 from django.db.models.signals import post_save
+
+from accounts.models import GuestEmail
+
 User = settings.AUTH_USER_MODEL
 # fulano@mail.com -> pode ter 1.000.000.000 billing profiles
 # user fulano@mail.com -> pode ter apenas 1 billing profile
+class BillingProfileManager(models.Manager):
+    def new_or_get(self, request):
+        user = request.user
+        guest_email_id = request.session.get('guest_email_id')
+        created = False
+        obj = None
+        if user.is_authenticated:
+            'logged in user checkout; remember payment stuff'
+            obj, created = self.model.objects.get_or_create(
+                            user=user, email=user.email)
+        elif guest_email_id is not None:
+            'guest user checkout; auto reloads payment stuff'
+            guest_email_obj = GuestEmail.objects.get(id=guest_email_id)
+            obj, created = self.model.objects.get_or_create(
+                                            email=guest_email_obj.email)
+        else:
+            pass
+        return obj, created
+
 class BillingProfile(models.Model):
     user = models.OneToOneField(User, null = True, blank = True, on_delete = models.CASCADE)
     email = models.EmailField()
@@ -11,7 +33,7 @@ class BillingProfile(models.Model):
     update = models.DateTimeField(auto_now = True)
     timestamp = models.DateTimeField(auto_now_add = True)
     # customer_id no Stripe ou Braintree ou ...
-
+    objects = BillingProfileManager()
     def __str__(self):
         return self.email
 
