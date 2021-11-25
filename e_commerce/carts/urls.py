bf34e0c42d27dc513f093c3513da0be18384792d
toticavalcanti <toticavalcanti@hotmail.com>
2021-11-25 18:11:32 -0300

    order summary and success page

--- e_commerce/carts/urls.py
+++ e_commerce/carts/urls.py
@@ -6,10 +6,12 @@ from .views import (
                         cart_home, 
                         checkout_home,
                         cart_update,
+                        checkout_done_view
                     )
 
 urlpatterns = [
     path('', cart_home, name='home'),
+    path('checkout/success/', checkout_done_view, name='success'),
     path('checkout/', checkout_home, name='checkout'),
     path('update/', cart_update, name='update')
 ]
--- e_commerce/carts/views.py
+++ e_commerce/carts/views.py
@@ -67,7 +67,8 @@ def checkout_home(request):
             order_obj.mark_paid()
             request.session['cart_items'] = 0
             del request.session['cart_id']
-            return redirect("/cart/success")
+            return redirect("cart:success")
+    
     context = {
         "object": order_obj,
         "billing_profile": billing_profile,
@@ -77,3 +78,6 @@ def checkout_home(request):
         "address_qs": address_qs,
     }
     return render(request, "carts/checkout.html", context)
+
+def checkout_done_view(request):
+    return render(request, "carts/checkout-done.html", {})
