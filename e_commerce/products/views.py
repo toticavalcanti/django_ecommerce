8273d029830338d00b0bc3dfb36dc98640a3f1c0
toticavalcanti <toticavalcanti@hotmail.com>
2022-10-13 18:08:04 -0300

    add button in list products page

--- e_commerce/products/views.py
+++ e_commerce/products/views.py
@@ -28,6 +28,12 @@ class ProductListView(ListView):
     #     print(context)
     #     return context
 
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductListView,  self).get_context_data(*args, **kwargs)
+        cart_obj, new_obj = Cart.objects.new_or_get(self.request)
+        context['cart'] = cart_obj
+        return context
+
 #Function Based View
 def product_list_view(request):
     queryset = Product.objects.all()
