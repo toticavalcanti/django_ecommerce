a565ff8a5b3179bba3291c517378f037b76a2718
toticavalcanti <toticavalcanti@hotmail.com>
2022-11-19 11:54:16 -0300

    auto search merge

--- e_commerce/products/views.py
+++ e_commerce/products/views.py
@@ -22,11 +22,16 @@ class ProductListView(ListView):
     #traz todos os produtos do banco de dados sem filtrar nada 
     queryset = Product.objects.all()
     template_name = "products/list.html"
-
+    
     # def get_context_data(self, *args, **kwargs):
     #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
     #     print(context)
     #     return context
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductListView,  self).get_context_data(*args, **kwargs)
+        cart_obj, new_obj = Cart.objects.new_or_get(self.request)
+        context['cart'] = cart_obj
+        return context
 
 #Function Based View
 def product_list_view(request):
