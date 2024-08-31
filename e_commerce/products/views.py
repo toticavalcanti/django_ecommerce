cae5394da2f2c9b3b5d3169a93c47961dfa46fd1
toticavalcanti <toticavalcanti@hotmail.com>
2024-08-31 18:04:18 -0300

    implement calculate_order_amount

--- e_commerce/products/views.py
+++ e_commerce/products/views.py
@@ -1,5 +1,4 @@
 from django.http import Http404
-
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
 from analytics.models import ObjectViewed
@@ -11,36 +10,24 @@ class ProductFeaturedListView(ListView):
     template_name = "products/list.html"
     
     def get_queryset(self, *args, **kwargs):
-        request = self.request
         return Product.objects.featured()
 
 class ProductFeaturedDetailView(ObjectViewedMixin, DetailView):
     queryset = Product.objects.all().featured()
     template_name = "products/featured-detail.html"
 
-#Class Based View
+# Class Based View
 class ProductListView(ListView):
-    #traz todos os produtos do banco de dados sem filtrar nada 
     queryset = Product.objects.all()
     template_name = "products/list.html"
     
-    # def get_context_data(self, *args, **kwargs):
-    #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
-    #     print(context)
-    #     return context
     def get_context_data(self, *args, **kwargs):
-        context = super(ProductListView,  self).get_context_data(*args, **kwargs)
+        context = super(ProductListView, self).get_context_data(*args, **kwargs)
         cart_obj, new_obj = Cart.objects.new_or_get(self.request)
         context['cart'] = cart_obj
         return context
 
-    def get_context_data(self, *args, **kwargs):
-        context = super(ProductListView,  self).get_context_data(*args, **kwargs)
-        cart_obj, new_obj = Cart.objects.new_or_get(self.request)
-        context['cart'] = cart_obj
-        return context
-
-#Function Based View
+# Function Based View
 def product_list_view(request):
     queryset = Product.objects.all()
     context = {
@@ -78,16 +65,14 @@ class ProductDetailSlugView(ObjectViewedMixin, DetailView):
         # Caso o usuário seja anônimo, não cria o ObjectViewed
         return instance
 
-
-
-
-#Class Based View
+# Class Based View
 class ProductDetailView(ObjectViewedMixin, DetailView):
     template_name = "products/detail.html"
 
     def get_context_data(self, *args, **kwargs):
         context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
-        print(context)
+        cart_obj, new_obj = Cart.objects.new_or_get(self.request)
+        context['cart'] = cart_obj
         return context
 
     def get_object(self, *args, **kwargs):
@@ -97,10 +82,9 @@ class ProductDetailView(ObjectViewedMixin, DetailView):
             raise Http404("Esse produto não existe!")
         return instance
 
-#Function Based View
-def product_detail_view(request, pk = None, *args, **kwargs):
+# Function Based View
+def product_detail_view(request, pk=None, *args, **kwargs):
     instance = Product.objects.get_by_id(pk)
-    print(instance)
     if instance is None:
         raise Http404("Esse produto não existe!")
 
