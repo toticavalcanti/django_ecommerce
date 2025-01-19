154639c5abc866ce395808c2128fb8639c9ff740
toticavalcanti <toticavalcanti@hotmail.com>
2025-01-19 10:50:46 -0300

    refactor(templates): Simplify the dd_class filter logic for form fields - Removed unnecessary validation for BoundField type in dd_class filter - Simplified the function to directly apply CSS classes to form fields

--- e_commerce/accounts/templatetags/custom_tags.py
+++ e_commerce/accounts/templatetags/custom_tags.py
@@ -1,5 +1,4 @@
 from django import template
-from django.forms.boundfield import BoundField
 
 register = template.Library()
 
@@ -8,7 +7,4 @@ def add_class(field, css_class):
     """
     Adiciona uma classe CSS a um campo de formulário.
     """
-    if isinstance(field, BoundField):
-        return field.as_widget(attrs={"class": css_class})
-    # Caso o campo não seja válido, retorna o campo original.
-    return field
+    return field.as_widget(attrs={"class": css_class})
